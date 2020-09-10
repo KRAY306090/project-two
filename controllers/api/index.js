c385373a2688d18124acc4723616d96fdbfad78e
Kennadee <kennadee.ray@gmail.com>
2020-09-09 20:34:53 -0500

    bug fix

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes.js');
-const postRoutes = require('./post-routes');
-const commentRoutes = require('./comment-routes');
-const voteRoutes = require("./vote-routes.js");
+// const userRoutes = require('./user-routes.js');
+// const postRoutes = require('./post-routes');
+// const commentRoutes = require('./comment-routes');
+// const voteRoutes = require("./vote-routes.js");
 
-router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
-router.use('/comments', commentRoutes);
-router.use('/votes', voteRoutes);
+// router.use('/users', userRoutes);
+// router.use('/posts', postRoutes);
+// router.use('/comments', commentRoutes);
+// router.use('/votes', voteRoutes);
 
 module.exports = router;
