0f4182e1b6dfb4a9ba234f5542e82e8902bb1aec
Kennadee <kennadee.ray@gmail.com>
2020-09-11 17:14:37 -0500

    submit button functionality added

--- public/javascript/new-recipe.js
+++ public/javascript/new-recipe.js
@@ -8,16 +8,41 @@ function addIngredientHandler(event) {
     const measurement = document.querySelector('#measurement').value.trim();
     const unit = document.querySelector('#unit').value.trim();
     const ingredientName = document.querySelector('#ingredient-name').value.trim();
-   
-     const newIngredient = measurement + " " + unit + " " + ingredientName;
-    
-     ingredientArr.push(newIngredient);
-     console.log(ingredientArr);
+
+    const newIngredient = measurement + " " + unit + " " + ingredientName;
+
+    ingredientArr.push(newIngredient);
+    console.log(ingredientArr);
 }
 
-// function takes array of ingredients and turns it into string to pass to database
-
 // function posts new recipe to database
+async function postRecipeHandler(event) {
+    event.preventDefault();
+
+    // takes array of ingredients and turns it into string
+    const stringed = ingredientArr.join(", ");
+    const title = document.querySelector('#name').value.trim();
+    const instructions = document.querySelector('#instructions').value.trim();
+
+    const response = await fetch('/api/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            stringed,
+            instructions
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    if (response.ok) {
+        document.location.replace('dashboard');
+    }
+    else {
+        alert(response.statusText);
+    }
+}
 
 // click listener calls function above
 document.querySelector('#add-ingredient').addEventListener('click', addIngredientHandler);
+document.querySelector('#submit').addEventListener('click', postRecipeHandler);
