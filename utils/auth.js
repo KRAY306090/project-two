df8bdc2841b36b4ac006f8f2590f0e920df04214
Vox Humana <jonprine@gmail.com>
2020-09-15 16:35:40 -0500

    changed styling on enter page

--- utils/auth.js
+++ utils/auth.js
@@ -2,7 +2,7 @@ const passportAuth = (req, res, next) => {
     if (req.user) {
         return next();
     }
-    return res.redirect('/');
+    return res.redirect('/enter');
 };
 
 module.exports = passportAuth;
