fea4d434348f238dbb1590a21d7bd6ee1846bfe5
Vox Humana <jonprine@gmail.com>
2020-09-15 11:37:42 -0500

    error in callbackurl - changed to 3001

--- utils/passport.js
+++ utils/passport.js
@@ -64,7 +64,7 @@ function(accessToken, refreshToken, profile, cb) {
 passport.use(new FacebookStrategy({
   clientID: process.env.FACEBOOK_APP_ID,
   clientSecret: process.env.FACEBOOK_APP_SECRET,
-  callbackURL: "http://localhost:3000/auth/facebook/callback"
+  callbackURL: "http://localhost:3001/auth/facebook/callback"
 },
 function(accessToken, refreshToken, profile, cb) {
   console.log(profile);
