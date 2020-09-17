b96afbabca158c7120b6ca1c00f4da678d0f5dcb
Vox Humana <jonprine@gmail.com>
2020-09-17 13:16:25 -0500

    moved const in homepage get route down

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -33,7 +33,7 @@ router.get("/", (req, res) => {
     ],
   })
     .then((dbPostData) => {
-      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      
 
       let loginStatus;
       if (typeof req.session.passport != "undefined") {
@@ -42,6 +42,7 @@ router.get("/", (req, res) => {
       } else {
         loginStatus = false;
       }
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
 
       res.render("homepage", {
         posts,
@@ -88,46 +89,52 @@ router.get("/dashboard", (req, res) => {
   res.render("dashboard");
 });
 
-router.get("/post/:id", (req, res) => {
-  Post.findOne({
-    where: {
-      id: req.params.id,
-    },
-    attributes: ["id", "post_text", "title", "created_at"],
-    include: [
-      {
-        model: Comment,
-        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
-        include: {
-          model: User,
-          attributes: ["username"],
-        },
-      },
-      {
-        model: User,
-        attributes: ["username"],
-      },
-    ],
-  })
-    .then((dbPostData) => {
-      if (!dbPostData) {
-        res.status(404).json({ message: "No post found with this id" });
-        return;
-      }
+// router.get("/post/:id", (req, res) => {
+//   Post.findOne({
+//     where: {
+//       id: req.params.id,
+//     },
+//     attributes: ["id", "post_text", "title", "created_at"],
+//     include: [
+//       {
+//         model: Comment,
+//         attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+//         include: {
+//           model: User,
+//           attributes: ["username"],
+//         },
+//       },
+//       {
+//         model: User,
+//         attributes: ["username"],
+//       },
+//     ],
+//   })
+//     .then((dbPostData) => {
+//       if (!dbPostData) {
+//         res.status(404).json({ message: "No post found with this id" });
+//         return;
+//       }
+//       let loginStatus;
+//       if (typeof req.session.passport != 'undefined') {
+//         loginStatus = req.session.passport.user;
+//       } else {
+//         loginStatus = false;
+//       }
 
-      // serialize the data
-      const post = dbPostData.get({ plain: true });
+//       // serialize the data
+//       const post = dbPostData.get({ plain: true });
 
-      // pass data to template
-      res.render("single-post", {
-        post,
-        loggedIn: req.session.loggedIn,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+//       // pass data to template
+//       res.render("single-post", {
+//         post,
+//         loggedIn: req.session.loggedIn,
+//       });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
 
 module.exports = router;
