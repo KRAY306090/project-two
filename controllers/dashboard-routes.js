2b588baee6e781c2b4c87027f1701a656c747f77
Vox Humana <jonprine@gmail.com>
2020-09-16 23:59:24 -0500

    removed style.css from main

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,93 +1,95 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
-const { session } = require('passport');
-const passportAuth = require('../utils/auth');
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
+const { session } = require("passport");
+const passportAuth = require("../utils/auth");
 
-
-router.get('/', passportAuth,  (req, res) => {
+router.get("/", passportAuth, (req, res) => {
   Post.findAll({
     where: {
       // use the ID from the session
       //id: req.params.id
-      user_id: req.session.passport.user.id
+      user_id: req.session.passport.user.id,
     },
-    attributes: [
-      'id', 'comment_text', 'post_id', 'user_id', 'created_at'
+    attributes: [      
+    'id',
+    'title',
+    'ingredients',
+    'post_text',
+    'category',
+    'user_id',
+    'created_at'
+  ],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
     ],
-    // include: [
-    //   {
-    //     model: Comment,
-    //     attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-    //     include: {
-    //       model: User,
-    //       attributes: ['username']
-    //     }
-    //   },
-    //   {
-    //     model: User,
-    //     attributes: ['username']
-    //   }
-    // ]
   })
-    .then(dbPostData => {
+    .then((dbPostData) => {
       // serialize data before passing to template
-      const posts = dbPostData.map(post => post.get({ plain: true }));
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
       let loginStatus;
-      if (typeof req.session.passport != 'undefined') {
-        loginStatus =
-        req.session.passport.user;
+      if (typeof req.session.passport != "undefined") {
+        loginStatus = req.session.passport.user;
       } else {
         loginStatus = false;
       }
-      res.render('dashboard', { posts, loggedin: loginStatus });
+      res.render("dashboard", {
+        posts,
+        loggedin: loginStatus,
+      });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
-  router.get('/edit/:id', passportAuth, (req, res) => {
-    Post.findOne({
-        where: {
-            user_id: req.session.user_id
+router.get("/edit/:id", passportAuth, (req, res) => {
+  Post.findOne({
+    where: {
+      user_id: req.session.user_id,
+    },
+    attributes: ["id", "post_text", "title", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
         },
-        attributes: [
-            'id',
-            'post_text',
-            'title',
-            'created_at'
-        ],
-        include: [
-            {
-                model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
-    })
-        .then(dbPostData => {
-          const posts = dbPostData.map(post => post.get({ plain: true }));
-            let loginStatus;
-              if (typeof req.session.passport != 'undefined') {
-                loginStatus = false;
-              }
-              console.log(loginStatus);
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      let loginStatus;
+      if (typeof req.session.passport != "undefined") {
+        loginStatus = false;
+      }
+      console.log(loginStatus);
 
-            res.render('edit-post', { post, loggedIn: true });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-})
-  
+      res.render("edit-post", { post, loggedIn: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
