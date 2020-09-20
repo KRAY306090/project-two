a505f04a92288a0bde0ce7ceb57211e23695e365
Vox Humana <jonprine@gmail.com>
2020-09-19 20:11:58 -0500

    home routes

--- controllers/index.js
+++ controllers/index.js
@@ -6,7 +6,7 @@ const dashboardRoutes = require('./dashboard-routes.js')
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
-router.use('/dashboard', dashboardRoutes);
+router.use('/', dashboardRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
