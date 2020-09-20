b87567bdc11965f93def358d888b0aa08c9d3d16
Vox Humana <jonprine@gmail.com>
2020-09-20 09:15:16 -0500

    removed session from const in dash routes

--- controllers/index.js
+++ controllers/index.js
@@ -6,7 +6,7 @@ const dashboardRoutes = require('./dashboard-routes.js')
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
-router.use('/', dashboardRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
--- utils/passport.js
+++ utils/passport.js
@@ -1,8 +1,9 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const { User } = require("../models");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const FacebookStrategy = require("passport-facebook").Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+
 
 // Passport local strategy login
 passport.use(
@@ -22,7 +23,7 @@ passport.use(
 
       let verify;
       if (user != null) {
-        console.log(user);
+          console.log(user);
         verify = await user.checkPassword(password);
       }
 
@@ -36,56 +37,41 @@ passport.use(
   )
 );
 
-passport.serializeUser(function (user, done) {
-  done(null, user.id);
+
+passport.serializeUser((user, cb) => {
+  console.log(user);
+  cb(null, user);
 });
 
-passport.deserializeUser(function (id, done) {
-  User.findById(id).then((user) => {
-    done(null, user);
-  });
+passport.deserializeUser((obj, cb) => {
+  cb(null, obj);
 });
 
 // Google OAuth
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "http://localhost:3001/auth/google/callback",
-      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
-    },
-    function (accessToken, refreshToken, profile, cb) {
-      console.log(profile);
-      User.findOrCreate({
-        where: { googleId: profile.id },
-        defaults: { username: profile.displayName },
-      })
-        .then((user) => cb(null, user))
-        .catch((err) => cb(err, null));
-    }
-  )
-);
+passport.use(new GoogleStrategy({
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: 'http://localhost:3001/auth/google/callback',
+  userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
+},
+function(accessToken, refreshToken, profile, cb) {
+  console.log(profile);
+  User.findOrCreate({where: { googleId: profile.id } , defaults: {username: profile.displayName}}).then(user => cb(null, user)).catch(err => cb(err, null))
+}
+));
 
 // Facebook OAuth
 
-passport.use(
-  new FacebookStrategy(
-    {
-      clientID: process.env.FACEBOOK_APP_ID,
-      clientSecret: process.env.FACEBOOK_APP_SECRET,
-      callbackURL: "http://localhost:3001/auth/facebook/callback",
-    },
-    function (accessToken, refreshToken, profile, cb) {
-      console.log(profile);
-      User.findOrCreate({
-        where: { facebookId: profile.id },
-        defaults: { username: profile.displayName },
-      })
-        .then((user) => cb(null, user))
-        .catch((err) => cb(err, null));
-    }
-  )
-);
+passport.use(new FacebookStrategy({
+  clientID: process.env.FACEBOOK_APP_ID,
+  clientSecret: process.env.FACEBOOK_APP_SECRET,
+  callbackURL: "http://localhost:3001/auth/facebook/callback"
+},
+function(accessToken, refreshToken, profile, cb) {
+  console.log(profile);
+  User.findOrCreate({where: { facebookId: profile.id } , defaults: {username: profile.displayName}}).then(user => cb(null, user)).catch(err => cb(err, null))
+}
+));
+
 
 module.exports = passport;
