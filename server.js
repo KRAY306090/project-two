e98336be51ce0c1d63788d66ee1bde5ee31d1735
Vox Humana <jonprine@gmail.com>
2020-09-20 22:07:57 -0500

    up secret

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-  secret: 'abcdefg',
+  secret: 'superSecretSecret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
